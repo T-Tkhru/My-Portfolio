701498693657ec4e35cffab58f0f0d8fb87d77d1
T-Tkhru <ttkirua1@yahoo.co.jp>
2024-10-09 18:16:58 +0900

    footerにGitリンク追加、ついでに別タブで開くように

--- app/compornents/Footer.tsx
+++ app/compornents/Footer.tsx
@@ -10,7 +10,11 @@ const Footer = (props: FooterProps) => {
     <footer className={`${bgcolor} border-t-2 h-32 relative`}>
       <ul className="flex h-30 space-x-10 pt-8 justify-center">
         <li>
-          <Link href="https://x.com/TaKadayoooooo">
+          <Link
+            href="https://x.com/TaKadayoooooo"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img
               src="/X-logo.svg"
               alt="X Logo"
@@ -19,10 +23,27 @@ const Footer = (props: FooterProps) => {
           </Link>
         </li>
         <li>
-          <Link href="https://www.instagram.com/acril0530/">
+          <Link
+            href="https://www.instagram.com/acril0530/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img
               src="/Instagram-logo.svg"
               alt="Instagram Logo"
+              className="mix-blend-difference h-8 hover:opacity-70 duration-300"
+            />
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="https://github.com/T-Tkhru"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="/github-mark-white.svg"
+              alt="GitHub Logo"
               className="mix-blend-difference h-8 hover:opacity-70 duration-300"
             />
           </Link>
